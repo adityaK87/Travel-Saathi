5550e806b202bf949cc77b075dd4a53816e295d3
Aditya Kumar <adityapal1409@gmail.com>
2023-07-17 21:41:29 +0530

    implement searchComponent in the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import Map from "./component/map/index";
-function App() {
-	return <Map />;
-}
+import SearchComponent from "./component/searchField/index";
 
+import { placeSearch } from "./api";
+
+function App() {
+	const [marker, setMarker] = useState({ lat: 51.505, lng: -0.09 });
+	const [location, setLocation] = useState("New York");
+
+	const [places, setPlaces] = useState();
+
+	useEffect(() => {
+		placeSearch(marker).then((placesData) => {
+			setPlaces(placesData.results);
+		});
+	}, [marker]);
+
+	return (
+		<div className='App flex justify-space-between'>
+			<SearchComponent setLocation={setLocation} places={places} />
+			<Map places={places} marker={marker} setMarker={setMarker} />{" "}
+			{/** Map component will contain the map shown in our React app */}
+		</div>
+	);
+}
 export default App;
