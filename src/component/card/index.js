b009d97d19f408435beab4c218af0e478e3f2727
Aditya Kumar <adityapal1409@gmail.com>
2023-07-19 03:02:26 +0530

    implement imageSlider component to card component

--- src/component/card/index.js
+++ src/component/card/index.js
@@ -1,90 +1,62 @@
 import React, { useEffect, useState } from "react";
-import Image from "../../img/hotel.jpeg";
 import { getPlaceImages, getPlaceTips } from "../../api";
 import { BiSolidMessageSquareDetail } from "react-icons/bi";
-import { AiOutlineComment, AiOutlineFieldTime } from "react-icons/ai";
+import { AiOutlineFieldTime } from "react-icons/ai";
 import { IoLocationSharp } from "react-icons/io5";
+import ImageSlider from "../imageSlider";
+
 const Card = ({ place }) => {
-	// const [cardDetails, setCardDetails] = useState();
-	const [image, setImage] = useState([]);
+	const [images, setImages] = useState([]);
 	const [tip, setTip] = useState("");
-	const {
-		name,
-		location,
-		geocodes,
-		timezone,
-		categories,
-		link,
-		fsq_id,
-		related_places,
-	} = place;
-	const {
-		address,
-		city,
-		country,
-		dma,
-		postcode,
-		cross_street,
-		formatted_address,
-		locality,
-	} = location;
-	const [first_obj] = categories;
-	// console.log("first_obj", first_obj);
+
+	//Destructuring the object place and location
+	const { name, location, timezone, categories, fsq_id } = place;
+	const { formatted_address } = location;
 
 	useEffect(() => {
 		getPlaceImages(fsq_id).then((imageObj) => {
-			setImage(imageObj);
+			setImages(imageObj);
 		});
+
+		//getPlaceTips is defined in the api folder
 		getPlaceTips(fsq_id).then((tip) => {
-			const newTip = tip?.slice(0, 25);
+			const newTip = tip?.slice(0, 80);
 			setTip(newTip);
 		});
-	}, []);
+	}, [name]);
 
-	const { icon } = first_obj;
-	const [first_image] = image;
-	console.log("image", image);
-	// console.log("tip", tip);
-
-	// console.log("first_image", first_image);
 	return (
 		<div className='w-30 max-w-sm rounded overflow-hidden shadow-lg m-4 my-8 hover:shadow-md hover:shadow-slate-600 hover:scale-105 hover:duration-300'>
-			<img
-				className='w-full '
-				src={Image}
-				alt='Sunset in the mountains'
-			/>
+			<ImageSlider images={images} />
 			<div className='font-bold text-xl mb-2 px-6 py-2'>{name}</div>
 			<div className='px-6 py-2 '>
 				<p className='text-gray-700 text-base flex justify-between flex items-center'>
-					<span className='w-4'>
+					<span className='w-4 mr-14'>
 						<IoLocationSharp />
 					</span>
-					<span>{formatted_address}</span>
+					<span className=''>{formatted_address}</span>
 				</p>
 			</div>
-			<div className='px-6 py-2 '>
-				<p className='text-gray-700 text-base flex justify-between flex items-center'>
-					<IoLocationSharp />
-					<span>
-						{address}, {locality}
+
+			<div className='px-6 py-2'>
+				<p className='text-gray-700 text-base flex justify-between flex items-center '>
+					<span className='w-4 mr-14'>
+						<BiSolidMessageSquareDetail />
 					</span>
+					<span>{tip}...</span>
 				</p>
 			</div>
 			<div className='px-6 py-2'>
-				<p className='text-gray-700 text-base flex justify-between flex items-center'>
-					<BiSolidMessageSquareDetail />
-					{tip}...
-				</p>
-			</div>
-			<div className='px-6 py-2'>
-				<p className='text-gray-700 text-base flex justify-between flex items-center'>
-					<AiOutlineFieldTime /> {timezone}
+				<p className='text-gray-700 text-base flex  items-center'>
+					<span className='w-4 mr-14'>
+						<AiOutlineFieldTime />
+					</span>
+					<span>{timezone}</span>
 				</p>
 			</div>
 
 			<div className='px-6 pt-4 pb-2'>
-				{categories.map((category, index) => {
+				{categories?.map((category, index) => {
 					return (
 						<span
 							key={index}
@@ -94,6 +66,13 @@ const Card = ({ place }) => {
 					);
 				})}
 			</div>
+			<a
+				className='font-semibold px-6 pt-4 pb-2 text-sky-400'
+				href={`https://foursquare.com/v/${name}/${fsq_id}`}
+				target={"_blank"}
+				rel='noreferrer'>
+				WEBSITE
+			</a>
 		</div>
 	);
 };
