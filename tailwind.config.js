11fbad850f33167a340910b129ad6505f8983020
Aditya Kumar <adityapal1409@gmail.com>
2023-07-19 02:50:04 +0530

    update config file for @material-tailwind/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{html,js}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-};
+});
