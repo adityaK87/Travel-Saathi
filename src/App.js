c713d15362d9a5fa2a94b860a36e4d1096593f30
Aditya Kumar <adityapal1409@gmail.com>
2023-07-19 02:55:07 +0530

    implement navbar component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Map from "./component/map/index";
-import SearchComponent from "./component/searchField/index";
-
+import SideMenu from "./component/sideMenu";
+import Navbar from "./component/navbar";
 import { placeSearch } from "./api";
+import "./styles/tailwind.css";
 
 function App() {
 	const [marker, setMarker] = useState({ lat: 51.505, lng: -0.09 });
-	const [location, setLocation] = useState("New York");
-
+	const [selectedCategory, setSelectedCategory] = useState("Hotels");
 	const [places, setPlaces] = useState();
 
 	useEffect(() => {
-		placeSearch(marker).then((placesData) => {
+		placeSearch(marker, selectedCategory).then((placesData) => {
 			setPlaces(placesData.results);
 		});
-	}, [marker]);
+
+		// filterCategory(selectedCategory);
+	}, [marker, selectedCategory]);
 
 	return (
-		<div className='App flex justify-space-between'>
-			<SearchComponent setLocation={setLocation} places={places} />
-			<Map places={places} marker={marker} setMarker={setMarker} />{" "}
-			{/** Map component will contain the map shown in our React app */}
-		</div>
+		<>
+			<Navbar
+				selectedCategory={selectedCategory}
+				setSelectedCategory={setSelectedCategory}
+			/>
+			<div className='App flex justify-space-between'>
+				<SideMenu
+					setSelectedCategory={setSelectedCategory}
+					places={places}
+				/>
+				<Map
+					places={places}
+					marker={marker}
+					setMarker={setMarker}
+					selectedCategory={selectedCategory}
+				/>
+				{/** Map component will contain the map shown in our React app */}
+			</div>
+		</>
 	);
 }
 export default App;
