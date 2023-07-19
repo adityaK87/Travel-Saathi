b2c47e27b05eb96fc22ce07b269a4f276021c833
Aditya Kumar <adityapal1409@gmail.com>
2023-07-19 13:44:14 +0530

    implement image in card chip

--- src/component/card/index.js
+++ src/component/card/index.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { getPlaceImages, getPlaceTips } from "../../api";
+import { getPlaceTips } from "../../api";
 import { BiSolidMessageSquareDetail } from "react-icons/bi";
 import { AiOutlineFieldTime } from "react-icons/ai";
 import { IoLocationSharp } from "react-icons/io5";
 import ImageSlider from "../imageSlider";
 
 const Card = ({ place }) => {
-	const [images, setImages] = useState([]);
 	const [tip, setTip] = useState("");
 
 	//Destructuring the object place and location
@@ -14,20 +13,16 @@ const Card = ({ place }) => {
 	const { formatted_address } = location;
 
 	useEffect(() => {
-		getPlaceImages(fsq_id).then((imageObj) => {
-			setImages(imageObj);
-		});
-
 		//getPlaceTips is defined in the api folder
 		getPlaceTips(fsq_id).then((tip) => {
 			const newTip = tip?.slice(0, 80);
 			setTip(newTip);
 		});
-	}, [name]);
+	}, [fsq_id]);
 
 	return (
 		<div className='w-30 max-w-sm rounded overflow-hidden shadow-lg m-4 my-8 hover:shadow-md hover:shadow-slate-600 hover:scale-105 hover:duration-300'>
-			<ImageSlider images={images} />
+			<ImageSlider fsq_id={fsq_id} />
 			<div className='font-bold text-xl mb-2 px-6 py-2'>{name}</div>
 			<div className='px-6 py-2 '>
 				<p className='text-gray-700 text-base flex justify-between flex items-center'>
@@ -55,19 +50,25 @@ const Card = ({ place }) => {
 				</p>
 			</div>
 
-			<div className='px-6 pt-4 pb-2'>
-				{categories?.map((category, index) => {
+			<div className='px-6 py-2'>
+				{categories?.map(({ name, id, icon }) => {
+					//destructuring the category object
 					return (
 						<span
-							key={index}
-							className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-							{category.name}
+							key={id}
+							className='inline-block bg-gray-800 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2'>
+							<img
+								className='inline-block text-black'
+								src={`${icon.prefix}32${icon.suffix}`}
+								alt=''
+							/>
+							{name}
 						</span>
 					);
 				})}
 			</div>
 			<a
-				className='font-semibold px-6 pt-4 pb-2 text-sky-400'
+				className='font-semibold px-6 py-2 mb-2 text-sky-400 mb-4 hover:text-blue'
 				href={`https://foursquare.com/v/${name}/${fsq_id}`}
 				target={"_blank"}
 				rel='noreferrer'>
