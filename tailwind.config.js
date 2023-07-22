cf8181d52c56ef7397011d1efbdcf384310a37a3
Aditya Kumar <adityapal1409@gmail.com>
2023-07-22 16:19:40 +0530

    add different screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+
 	theme: {
-		extend: {},
+		screens: {
+			sm: "640px",
+			// rest of the breakpoints
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
 	},
 	plugins: [],
 });
